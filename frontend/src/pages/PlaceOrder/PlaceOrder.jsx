0746584f97bb223041d170708eb245c5a0f9ff51
catarinarodriguesv <catarinarodriguesv@gmail.com>
2025-09-06 15:35:36 +0100

    feature: create user order page

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -2,6 +2,8 @@ import React, { useState, useContext } from 'react'
 import './PlaceOrder.css'
 import { StoreContext } from '../../context/StoreContext'
 import axios from "axios"
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const PlaceOrder = () => {
 
@@ -55,6 +57,15 @@ const PlaceOrder = () => {
     }
   }
 
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    if (!token) {
+      navigate("/cart");
+    } else if(getTotalCartAmount()===0){
+      navigate("/cart");
+    }
+  },[token]);
 
 
   return (
